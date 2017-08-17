ee2cc81bfceb813eea4c69af296b4d682e0dc86e
unknown <1960772525@qq.com>
2017-08-17 18:38:40 +0800

    提交多个

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React,{Component} from 'react'
+
+
+export default class App extends Component{
+	constructor(){
+		this.state = {
+			
+		}
+	}
+	
+	render(){
+		return(
+			<div>首页</div>
+		)
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
